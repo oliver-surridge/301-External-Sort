2b4dddbdd5aea93e23078b23dc320057a624dcab
oliver-surridge <olivertsurridge@gmail.com>
2025-03-14 17:12:59 +1300

    added heap sort

--- XSort.java
+++ XSort.java
@@ -1,6 +1,73 @@
 public class XSort {
 
     public static void main(String[] args) {
-        
+        String[] text = {"banana", "apple", "cherry", "mango", "blueberry"};
+        BuildMaxHeap(text);
     }
+    /**
+     * Builds max heap from unsorted array
+     * @param array the array to be converted to a maxHeap
+     */
+    public static void BuildMaxHeap(String[] array){
+        int arrayLength = array.length;
+
+        //build max heap
+        for(int i = arrayLength/2 - 1; i >= 0; i--){
+            Heapify(array, arrayLength , i);
+        }
+
+        for(int i = arrayLength - 1; i > 0; i--){
+            //move the current root to the end of the array
+            Swap(array, 0, i);
+            //rebuild the heap
+            Heapify(array, i, 0);
+        }
+    }
+    /**
+     * rebuilds the heap
+     * @param array the array containing the heap
+     * @param n the size of the heap
+     * @param i the index of the subtree root to heapify
+     */
+    public static void Heapify(String[] array, int n, int i){
+        int largest = i; //initially set the largest as the root
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        //if the left child is larger than the root
+        if(leftChild < n && array[leftChild].compareTo(array[largest]) > 0){
+            largest = leftChild;
+        }
+
+        //if the right child is larger than the root
+        if(rightChild < n && array[rightChild].compareTo(array[largest]) > 0){
+            largest = rightChild;
+        }
+
+        //if the 'largest' variable has been changed
+        if(largest != i){
+            Swap(array, i, largest);
+            
+            //recursively heapify until the root is the largest 
+            Heapify(array, n, largest);
+        }
+
+    }
+    
+   /**
+    * Swaps two elements in the array
+    * @param array 
+    * @param firstElement index of first element
+    * @param secondElement index of second element
+    */
+    public static void Swap(String[] array, int firstElement, int secondElement){
+        String temp = array[firstElement];
+        array[firstElement] = array [secondElement];
+        array[secondElement] = temp;
+    }
+
+
+    
+
+
 }
