2e197f63290cfd5eee2e989a634e15dc4cc3aded
oliver-surridge <olivertsurridge@gmail.com>
2025-03-17 20:45:14 +1300

    added input validation

--- XSort.java
+++ XSort.java
@@ -1,8 +1,57 @@
 public class XSort {
 
     public static void main(String[] args) {
-        String[] text = {"banana", "apple", "cherry", "mango", "blueberry"};
-        BuildMaxHeap(text);
+        // String[] text = {"banana", "apple", "cherry", "mango", "blueberry"};
+        // BuildMaxHeap(text);
+
+        // //print the array
+        // for (String s : text) {
+        //     System.out.print(s + " ");
+        // }
+
+        //error checking and taking in inputs
+        if(args.length < 1 || args.length > 2){
+            System.err.println("Usage: java XSort [runSize] [mergeWay]");
+            System.exit(1);
+        }
+        
+        //validate the first input
+        int runSize = 0; //hold the runSize entered
+        try{
+            runSize = Integer.parseInt(args[0]);
+            if(runSize < 64 || runSize > 1024){
+                System.err.println("runSize must be between 64 and 1024");
+                System.exit(1);
+            }
+        } catch(NumberFormatException e){
+            System.err.println("runSize must be of an integer value");
+            System.exit(1);
+        }
+
+        //validate the second input
+        boolean mergeWay = false; //if mergeWay entered is not 2, we cannot merge
+        if(args.length == 2){
+            if(args[1].equals("2")){
+                mergeWay = true;
+            } else{
+                System.err.println("mergeWay has only been implemented for 2 way");
+                System.exit(1);
+            }
+        }
+        
+        try {
+            createInitRuns(runSize);
+        } catch (Exception e) {
+            e.printStackTrace(); 
+        }        
+    }
+
+    /**
+     * produce runs for each line of size (runSize)
+     * @param runSize size for each run
+     */
+    public static void createInitRuns(int size) {
+        
     }
     /**
      * Builds max heap from unsorted array
@@ -11,7 +60,7 @@ public class XSort {
     public static void BuildMaxHeap(String[] array){
         int arrayLength = array.length;
 
-        //build max heap
+        // Build a max heap by heapifying from the last non leaf node up to the root.
         for(int i = arrayLength/2 - 1; i >= 0; i--){
             Heapify(array, arrayLength , i);
         }
