bc4aceaefb5aadd8e9e1c70e9f4ea92c346389e7
oliver-surridge <olivertsurridge@gmail.com>
2025-03-20 17:31:05 +1300

    Initial runs functionality working

--- XSort.java
+++ XSort.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class XSort {
 
     public static void main(String[] args) {
@@ -29,11 +34,12 @@ public class XSort {
         }
 
         //validate the second input
-        boolean mergeWay = false; //if mergeWay entered is not 2, we cannot merge
+        boolean mergeWay = false; 
         if(args.length == 2){
             if(args[1].equals("2")){
                 mergeWay = true;
             } else{
+                //if mergeWay entered is not 2, we cannot merge
                 System.err.println("mergeWay has only been implemented for 2 way");
                 System.exit(1);
             }
@@ -41,18 +47,58 @@ public class XSort {
         
         try {
             createInitRuns(runSize);
-        } catch (Exception e) {
-            e.printStackTrace(); 
-        }        
+            //Merge
+            if(mergeWay){
+                System.err.println("Merging not yet implemented");
+            }
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+            System.exit(1);
+        }     
     }
 
     /**
      * produce runs for each line of size (runSize)
      * @param runSize size for each run
      */
-    public static void createInitRuns(int size) {
-        
+    public static void createInitRuns(int size) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        ArrayList<String> buffer = new ArrayList<>(); //used to temp store text for a run
+
+        //infinite while loop
+        while(true){
+
+            //clear the buffer for a new run
+            buffer.clear();
+
+            //read (size) amount of lines 
+            while(buffer.size() < size){
+                line = reader.readLine();
+                //null rather than "" means we have reached the end of the input
+                if(line == null){
+                    break;
+                }
+                //add the line to the buffer array
+                buffer.add(line);
+            }
+            //create the run
+            String[] runArray = buffer.toArray(new String[0]);
+
+            //sort the run
+            BuildMaxHeap(runArray);
+
+            //output the run
+            for(String sortedLine : runArray){
+                System.out.println(sortedLine);
+            }
+
+            if(line == null){
+                break;
+            }
+        }
     }
+
     /**
      * Builds max heap from unsorted array
      * @param array the array to be converted to a maxHeap
@@ -72,6 +118,7 @@ public class XSort {
             Heapify(array, i, 0);
         }
     }
+    
     /**
      * rebuilds the heap
      * @param array the array containing the heap
@@ -103,7 +150,7 @@ public class XSort {
 
     }
     
-   /**
+    /**
     * Swaps two elements in the array
     * @param array 
     * @param firstElement index of first element
